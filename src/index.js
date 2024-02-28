aa3906642034c116c7803fac98557791b8b6a785
kiboma <omayioben@gmail.com>
2024-02-28 15:09:45 +0300

    Install AN andnd connect firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Firebase by kiboma254")
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDY4K7mhnvVyTs-mvtavPmUkfH-NXTqQFA",
+  authDomain: "kiboma254-f4409.firebaseapp.com",
+  projectId: "kiboma254-f4409",
+  storageBucket: "kiboma254-f4409.appspot.com",
+  messagingSenderId: "189102561308",
+  appId: "1:189102561308:web:f866791db54e559dfc27d9"
+};
+
+const app = initializeApp(firebaseConfig);
